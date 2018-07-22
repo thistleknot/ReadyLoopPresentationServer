e41a53144059a30e87d4393dc08512102d65aeec
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-07-22 11:44:16 -0700

    cleaned up nasdaqlist import
    attempted pipe delimter import

--- populateDB.bat
+++ populateDB.bat
@@ -3,6 +3,7 @@ set PGPASSWORD=1234
 set APIKEY=|type apikey.txt
 set dbName=somedb
 set tableName=ur_table
+set pipe=|printf '\174'
 
 curl --silent "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=full&apikey=%APIKEY%&datatype=csv" --stderr -> %name%.csv
 
@@ -10,16 +11,22 @@ curl --silent "ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt" --st
 
 xcopy %name%.csv c:\test\ /y
 
-REM last line
+REM remove last line
 
 sed -i "$d" nasdaqlisted.txt
 
 REM ^^essential for |
 sed 's/^^/"/;s/|/;/g;s/$/"/' nasdaqlisted.txt > removedPipes.txt
 
+REM remove quotes
 sed 's/^^/"/;s/"//g;s/$//' removedPipes.txt > c:\test\nasdaqSymbols.csv
-erase nasdaqlisted.txt
-erase removedPipes.txt
+REM sed 's/^^/"/;s/"//g;s/$//' nasdaqlisted.txt > c:\test\nasdaqSymbols.txt
+
+REM erase nasdaqlisted.txt
+REM erase removedPipes.txt
+
+REM add symbol
+awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%name% MSFT.csv > test.txt
 
 echo drop database if exists nasdaqSymbols; create database nasdaqsymbols;| psql -U postgres
 
@@ -35,13 +42,22 @@ REM symbol,securityName,MarketCategory,testIssue,financialStatus,roundLotSize,ET
 
 echo COPY nSymbols(symbol,securityName,MarketCategory,testIssue,financialStatus,roundLotSize,ETF,nextShares) FROM 'c:\test\nasdaqSymbols.csv' DELIMITER ';' CSV HEADER;| psql -U postgres nasdaqsymbols
 
-echo drop database if exists %dbName%; create database %dbName%;| psql -U postgres
-echo drop table if exists public.%tableName%; CREATE TABLE public.%tableName% (timestamp date, open real, high real,low real,close real,adjusted_close real,volume real,dividend_amount real,split_coefficient real,CONSTRAINT timestamp_pkey PRIMARY KEY (timestamp)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE public.%tableName% OWNER to postgres; COPY %tableName%(timestamp,open,high,low,close,adjusted_close,volume,dividend_amount,split_coefficient) FROM 'c:\test\%name%.csv' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
+REM echo COPY nSymbols(symbol,securityName,MarketCategory,testIssue,financialStatus,roundLotSize,ETF,nextShares) FROM 'c:\test\nasdaqSymbols.txt' CSV HEADER --fields-terminated-by="|";| psql -U postgres nasdaqsymbols
 
-echo ALTER TABLE %tableName% ADD COLUMN symbol varchar(8) DEFAULT '%name%';| psql -U postgres %dbName%
+REM specific symbol
+
+echo drop database if exists %dbName%; create database %dbName%;| psql -U postgres
+echo drop table if exists public.%tableName%; | psql -U postgres %dbName%
+
+REM CREATE TABLE public.%tableName% (timestamp date, open real, high real,low real,close real,adjusted_close real,volume real,dividend_amount real,split_coefficient real,CONSTRAINT timestamp_pkey PRIMARY KEY (timestamp)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE public.%tableName% OWNER to postgres; COPY %tableName%(timestamp,open,high,low,close,adjusted_close,volume,dividend_amount,split_coefficient) FROM 'c:\test\%name%.csv' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
+
+echo CREATE TABLE public.%tableName% (symbol varchar(8), timestamp date, open real, high real,low real,close real,adjusted_close real,volume real,dividend_amount real,split_coefficient real,CONSTRAINT timestamp_pkey PRIMARY KEY (timestamp,symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE public.%tableName% OWNER to postgres; COPY %tableName%(symbol,timestamp,open,high,low,close,adjusted_close,volume,dividend_amount,split_coefficient) FROM 'c:\test\test.txt' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
+
+REM echo ALTER TABLE %tableName% ADD COLUMN symbol varchar(8) DEFAULT '%name%';| psql -U postgres %dbName%
 
 echo ALTER TABLE %tableName% DROP CONSTRAINT timestamp_pkey;| psql -U postgres %dbName%
 echo ALTER TABLE %tableName% ADD CONSTRAINT timestamp_pkey PRIMARY KEY (timestamp,symbol);| psql -U postgres %dbName%
 
-echo select * from %tableName%;| psql -U postgres %dbName%
-echo select * from nSymbols;| psql -U postgres nasdaqsymbols
+REM echo select * from %tableName%;| psql -U postgres %dbName%
+REM echo's all symbols
+echo select symbol FROM nSymbols;| psql -U postgres nasdaqsymbols
