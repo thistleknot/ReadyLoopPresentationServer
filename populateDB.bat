64490861faff05e7b376824d65d341cee4204535
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-07-23 21:48:52 -0700

    removed heinous database copy
    reduced time significantly

--- populateDB.bat
+++ populateDB.bat
@@ -54,29 +54,38 @@ REM try: "INSERT INTO load VALUES ('07/17/2018 23:55:00','EDT','WEST',61752,1893
 REM ON CONFLICT ^(timestamp, symbol^) DO NOTHING FROM 'c:\test\%awSymbols.csv' DELIMITER ',' CSV HEADER;|psql -U postgres somedb	
 REM try: https://stackoverflow.com/questions/13947327/to-ignore-duplicate-keys-during-copy-from-in-postgresql
 
+REM doesn't work in for loop
+echo CREATE TABLE temp_table (symbol varchar(8), timestamp date, open real, high real,low real,close real,adjusted_close real,volume real,dividend_amount real,split_coefficient real,CONSTRAINT temp_pkey PRIMARY KEY (timestamp,symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE temp_table OWNER to postgres; | psql -U postgres %dbName%
+
 REM prints each entry
 for /F "delims=;" %a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
-	if not exist "c:\test\%a.csv" curl --silent "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%a&outputsize=full&apikey=%APIKEY%&datatype=csv" --stderr -> c:\test\%a.csv;
-	if not exist "c:\test\%awSymbols.csv" set waitFlag='True' 
-	if exist "c:\test\%awSymbols.csv" set waitFlag='False'
-	if not exist "c:\test\%awSymbols.csv" awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%a c:\test\%a.csv > c:\test\%awSymbols.csv
+	REM if not exist "c:\test\%a.csv" curl --silent "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%a&outputsize=full&apikey=%APIKEY%&datatype=csv" --stderr -> c:\test\%a.csv;
+	curl --silent "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%a&outputsize=full&apikey=%APIKEY%&datatype=csv" --stderr -> c:\test\%a.csv;
+	REM if not exist "c:\test\%awSymbols.csv" set waitFlag=true
+	REM if exist "c:\test\%awSymbols.csv" set waitFlag=false
+	REM if not exist "c:\test\%awSymbols.csv" awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%a c:\test\%a.csv > c:\test\%awSymbols.csv
+	awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%a c:\test\%a.csv > c:\test\%awSymbols.csv
 	
-	echo drop table public.temp_table;| psql -U postgres somedb
-	echo create table temp_table as table ur_table;|psql -U postgres somedb
+	echo drop table temp_table2;| psql -U postgres somedb
+	
+	echo create table temp_table2 as table temp_table;|psql -U postgres somedb
 
 	REM echo CREATE TABLE public.temp_table (symbol varchar(8), timestamp date, open real, high real,low real,close real,adjusted_close real,volume real,dividend_amount real,split_coefficient real,CONSTRAINT temp_timestamp_pkey PRIMARY KEY (timestamp,symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE public.temp_table OWNER to postgres; | psql -U postgres somedb
 
 	REM echo copy temp_table(symbol,timestamp,open,high,low,close,adjusted_close,volume,dividend_amount,split_coefficient) from 'c:\test\AABAwSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres somedb
 	
-	echo copy temp_table from 'c:\test\%awSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres somedb
+	echo copy temp_table2 from 'c:\test\%awSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres somedb
 	
-	echo insert into ur_table select distinct * from public.temp_table ON CONFLICT DO NOTHING;| psql -U postgres somedb
+	echo insert into ur_table select distinct * from temp_table2 ON CONFLICT DO NOTHING;| psql -U postgres somedb
 	
-	echo drop table public.temp_table;| psql -U postgres somedb
+	echo drop table temp_table2;| psql -U postgres somedb
 	
-	if "%waitFlag%"=='True' (Sleep '11');
+	
+	REM if %waitFlag% equ true Sleep '11';
+	timeout 12
 
 )
+REM echo drop table temp_table;| psql -U postgres somedb
 
 
 echo COPY %tableName%^(symbol,timestamp,open,high,low,close,adjusted_close,volume,dividend_amount,split_coefficient^) FROM 'c:\test\%awSymbol.csv' DELIMITER ';' CSV HEADER;^| psql -U postgres %dbName%	
