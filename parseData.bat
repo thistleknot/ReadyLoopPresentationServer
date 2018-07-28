e6e6b6edcd5a1fa9c02ea3708c2028a813df08e4
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-07-27 17:27:38 -0700

    parseData.bat
    created separate batch to handle downloading stocks due to inability to set counter variables via cut/paste (may need to do a function call).  See test.bat

--- parseData.bat
+++ parseData.bat
@@ -1,28 +1,22 @@
-set PGPASSWORD=|type psqlPW.txt
+REM set PGPASSWPRD=1234
 setlocal enableextensions enabledelayedexpansion
-REM set PGPASSWORD=1234
-REM findstr /R /N "^" apikey.txt| find /C ":" > lines.txt
-REM sed -i "$d" lines.txt
-REM set lines=|type lines.txt
-REM Thank you: https://stackoverflow.com/questions/6359820/how-to-set-commands-output-as-a-variable-in-a-batch-file
 FOR /F "tokens=*" %%a in ('returnNumLines.bat apiKey.txt') do SET numKeys=%%a
 FOR /F "tokens=*" %%a in ('returnLine.bat %numKeys% apiKey.txt') do SET APIKEY=%%a
-rem set numKeys=|returnNumLines.bat apiKey.txt
-REM set APIKEY=|type apikey.txt
+FOR /F "tokens=*" %%a in ('returnLine.bat %numKeys% psqlPW.txt') do SET PGPASSWORD=%%a
 
 set dbName=somedb
 set tableName=ur_table
-
-REM counter
-REM set /A i=0
+set /a count = 1
 
 REM doesn't work in for loop, temp table holds symbol data before on distinct merge is performed over master table
 echo CREATE TABLE temp_table (symbol varchar(8), timestamp date, open real, high real,low real,close real,adjusted_close real,volume real,dividend_amount real,split_coefficient real,CONSTRAINT temp_pkey PRIMARY KEY (timestamp,symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE temp_table OWNER to postgres; | psql -U postgres %dbName%
 
-REM prints each entry
 for /F "delims=;" %%a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
-	
-	curl --silent "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%%a&outputsize=full&apikey=%APIKEY%&datatype=csv" --stderr -> c:\test\%%a.csv;
+
+	REM key counter
+	echo !count!
+
+	curl --silent "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%%a&apikey=%APIKEY%&datatype=csv" --stderr -> c:\test\%%a.csv;
 
 	awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%%a c:\test\%%a.csv > c:\test\%%awSymbols.csv
 	
@@ -36,10 +30,13 @@ for /F "delims=;" %%a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
 	
 	echo drop table temp_table2;| psql -U postgres somedb
 
-	REM if %waitFlag% equ true Sleep '11';
-	timeout 12
-	
+	if !count! == %numKeys% (set /a count = 0)
+	if not !count! == %numKeys% set /a count += 1
 	
 )
 
 echo drop table temp_table;| psql -U postgres somedb
+endlocal
+
+:increaseby1
+set /a "count+=1"
--- test3.bat
+++ test3.bat
@@ -1,10 +1,16 @@
 setlocal enableextensions enabledelayedexpansion
 set /a count = 1
 for /F "delims=;" %%a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
-	@echo off
-  set /a count += 1
-	@echo on
-  echo !count!
-  REM echo %%a
+	
+  
+	echo !count!
+@echo off
+	if !count! == 3 (set /a count = 0)
+	if not !count! == 3 set /a count += 1
+	
+@echo on
+  
+  
+	
 )
 endlocal
