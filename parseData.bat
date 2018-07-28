dc285b055e010a7bc80fdb7f592896cd3c936252
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-07-27 22:25:27 -0700

    moved location of insert
    added symbol to parseData.bat

--- parseData.bat
+++ parseData.bat
@@ -15,6 +15,7 @@ for /F "delims=;" %%a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
 	FOR /F "tokens=*" %%b in ('returnLine.bat !count! proxyList.txt') do SET PROXY=%%b
 	echo !APIKEY!
 	echo !PROXY!
+	echo %%a
 
 	set t1=!TIME!
 	echo !t1!
@@ -22,7 +23,19 @@ for /F "delims=;" %%a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
 	REM curl -x !PROXY! -L "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%%a&outputsize=full&apikey=!APIKEY!&datatype=csv" -o c:\test\%%a.csv;
 	
 	curl -L "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%%a&outputsize=full&apikey=!APIKEY!&datatype=csv" -o c:\test\%%a.csv;
+	
+	awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%%a c:\test\%%a.csv > c:\test\%%awSymbols.csv
+	
+	echo drop table temp_table2;| psql -U postgres %dbName%
+	
+	echo create table temp_table2 as table temp_table;|psql -U postgres %dbName%
 
+	echo copy temp_table2 from 'c:\test\%%awSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
+	
+	echo insert into %tableName% select distinct * from temp_table2 ON CONFLICT DO NOTHING;| psql -U postgres %dbName%
+	
+	echo drop table temp_table2;| psql -U postgres %dbName%
+	
 	set t2=!TIME!
 	echo !t2!
 	
@@ -43,19 +56,7 @@ for /F "delims=;" %%a in (c:\test\nasdaqSymbolsNoHeader.csv) do (
 	
 	echo !noZerosSeconds!
 	
-	set /a noZeroSecondsMinus12=12-!noZerosSeconds!
-	
-	awk '{print F,$1,$2,$3,$4,$5,$6,$7,$8,$9}' FS=, OFS=, F=%%a c:\test\%%a.csv > c:\test\%%awSymbols.csv
-	
-	echo drop table temp_table2;| psql -U postgres %dbName%
-	
-	echo create table temp_table2 as table temp_table;|psql -U postgres %dbName%
-
-	echo copy temp_table2 from 'c:\test\%%awSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
-	
-	echo insert into %tableName% select distinct * from temp_table2 ON CONFLICT DO NOTHING;| psql -U postgres %dbName%
-	
-	echo drop table temp_table2;| psql -U postgres %dbName%
+	set /a noZeroSecondsMinus12=12-!noZerosSeconds!	
 
 	if !count! == %numKeys% (set /a count = 0)
 	if not !count! == %numKeys% set /a count += 1
