132da8d5de92509fceef584c299cd89a488c057a
This is... DWARF FORTRESS! <laferrierejc@gmail.com>
2018-07-27 22:06:03 -0700

    misc cleanup of vars

--- test5.bat
+++ test5.bat
@@ -1,10 +1,12 @@
 REM SETLOCAL ENABLEDELAYEDEXPANSION
 FOR /F "tokens=*" %%a in ('numLines2.bat c:\test\aobc.csv') do SET numKeys=%%a
 REM echo %numKeys%
+
+
 FOR /L %%i IN (1,1,%numKeys%) DO (
 
 	REM ECHO %%i
 	returnLine.bat %%i c:\test\nasdaqSymbolsNoHeader.csv;
 
   
-)
+)	
--- vars.bat
+++ vars.bat
@@ -1,9 +1,9 @@
 setlocal enableextensions enabledelayedexpansion
 
-FOR /F "tokens=*" %%a in ('returnLine.bat %numKeys%') do SET APIKEY=%%a
 FOR /F "tokens=*" %%a in ('returnNumLines.bat apiKey.txt') do SET numKeys=%%a
 FOR /F "tokens=*" %%a in ('returnLine.bat 1 psqlPW.txt') do SET PGPASSWORD=%%a
 FOR /F "tokens=*" %%a in ('returnNumLines.bat c:\test\nasdaqSymbolsNoHeader.csv') do SET numNasdaqSymbols=%%a
 
 set dbName=readyloop
 set tableName=dadjclose
+ 
