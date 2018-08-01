9321cff1d2dae39810ed1b088507b18e8a1a75a4
My digital passions <laferrierejc@gmail.com>
2018-07-31 19:07:16 -0700

    line removal
    removed

--- insertBonds.bat
+++ insertBonds.bat
@@ -5,7 +5,6 @@ REM adds symbol to front of columns
 for /F "delims=," %%a in ('etfnamessymbols.bat') do (
 
 			awk '{print F,$1,$2,$3,$4,$5,$6,$7}' FS=, OFS=, F=%%a c:\test\share\etf-%%a.csv > c:\test\share\etf-%%awSymbols.csv
-			
 	
 			echo drop table if exists temp_table%%a;| psql -U postgres %dbName%
 	
