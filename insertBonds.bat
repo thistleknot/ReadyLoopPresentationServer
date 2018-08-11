4ffb29b16f0b1103067d80d58dbfe2f9df1a52cc
My digital passions <laferrierejc@gmail.com>
2018-08-10 17:25:38 -0700

    finally inserting lots of bonds
    confirming each visually as inserting

--- insertBonds.bat
+++ insertBonds.bat
@@ -2,6 +2,10 @@ REM Called by PopulateDB.bat after bonds have been downloaded.  Creates tables, 
 
 REM adds symbol to front of columns
 
+cd c:\test\share\etf\
+
+rename "etf-*.csv" "////*.csv"
+
 set dbName=readyloop
 
 set PGPASSWORD=1234
@@ -10,18 +14,21 @@ echo drop table if exists etf_bond_facts cascade;| psql -U postgres %dbName%
 
 echo CREATE TABLE IF NOT EXISTS etf_bond_facts AS select * from bond_facts_template;| psql -U postgres %dbName%	
 
-for /F %%a in (c:\test\ETFNamesSymbolsNoHeader.csv) do (
+dir c:\test\share\etf\*.csv /b > bondDirList.txt
+cut -f 1 -d . bondDirList.txt > bondList
 
-			awk '{print F,$1,$2,$3,$4,$5,$6,$7}' FS=, OFS=, F=%%a c:\test\share\etf\etf-%%a.csv > c:\test\share\etf\etf-%%awSymbols.csv
+for /F %%a in (bondList) do (
+
+			awk '{print F,$1,$2,$3,$4,$5,$6,$7}' FS=, OFS=, F=%%a c:\test\share\etf\%%a.csv > c:\test\share\etf\%%awSymbols.csv
 
 			echo drop table if exists temp_table%%a;| psql -U postgres %dbName%
 	
 			echo create table bond_facts%%a as table bond_facts_template;|psql -U postgres %dbName%
 
-			echo copy bond_facts%%a from 'c:\test\share\etf\etf-%%awSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
+			echo copy bond_facts%%a from 'c:\test\share\etf\%%awSymbols.csv' DELIMITER ',' CSV HEADER;| psql -U postgres %dbName%
 
 			echo insert into etf_bond_facts select distinct * from bond_facts%%a ON CONFLICT DO NOTHING;| psql -U postgres %dbName%
 
 			echo drop table bond_facts%%a;| psql -U postgres %dbName%
 	)
-	
+	erase bondDirList.txt
