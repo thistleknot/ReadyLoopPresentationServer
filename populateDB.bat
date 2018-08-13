40831106625eb502d821195137b75ff85eddb370
My digital passions <laferrierejc@gmail.com>
2018-08-12 21:42:58 -0700

    qs_facts

--- populateDB.bat
+++ populateDB.bat
@@ -135,9 +135,15 @@ REM create NASDAQ & Other (DOW and NYSE) fact tables
 	echo CREATE TABLE IF NOT EXISTS other_facts AS select * from other_facts_template;| psql -U postgres %dbName%
 
 REM create ETF-Bonds fact table	
-	echo CREATE TABLE IF NOT EXISTS public.etf_bond_facts_template (symbol varchar(8), timestamp date, open real null, high real null,low real null,close real null,adjusted_close real null,volume real null,CONSTRAINT etf_bond_facts_template_key PRIMARY KEY (timestamp,symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default;ALTER TABLE public.etf_bond_facts_template OWNER to postgres; | psql -U postgres %dbName%
+	echo CREATE TABLE IF NOT EXISTS public.etf_bond_facts_template (symbol varchar(8), timestamp date, open real null, high real null, low real null, close real null, adjusted_close real null, volume real null, CONSTRAINT etf_bond_facts_template_key PRIMARY KEY (timestamp,symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default; ALTER TABLE public.etf_bond_facts_template OWNER to postgres; | psql -U postgres %dbName%
 	
 	echo CREATE TABLE IF NOT EXISTS etf_bond_facts AS select * from public.etf_bond_facts_template;| psql -U postgres %dbName%
+
+Rem create qs_fact table
+	echo CREATE TABLE IF NOT EXISTS qs_facts_template (symbol varchar(8), timestamp date, close real null, open real null, high real null, low real null, volume real null, CONSTRAINT qs_facts_template_key PRIMARY KEY (timestamp, symbol)) WITH (OIDS=FALSE) TABLESPACE pg_default; ALTER TABLE qs_facts_template OWNER to postgres; | psql -U postgres %dbName%
+	
+	echo CREATE TABLE IF NOT EXISTS qs_facts AS select * from qs_facts_template;| psql -U postgres %dbName%
+
 	
 REM download data
 
