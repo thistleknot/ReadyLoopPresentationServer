fdc06f4def87ea4771660baf111f79843b91653b
My digital passions <laferrierejc@gmail.com>
2018-09-04 03:25:47 -0700

    read table sp500 addition

---
+++ read table sp500.R
@@ -0,0 +1,6 @@
+library(XML)
+library(RCurl)
+
+url <- "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+sp500html <- getURL(url)
+sp500 <- readHTMLTable(sp500html, stringsAsFactors = F)[[1]][,1]
