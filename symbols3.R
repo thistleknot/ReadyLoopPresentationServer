dab959d3ad23d8ac75eae85ffe17694fc15bb763
My digital passions <laferrierejc@gmail.com>
2020-03-19 13:53:26 -0600

    added coefficientbeta term to reduce while beta testing
    also added sample of main list to reduce cost of operation

--- symbols3.R
+++ symbols3.R
@@ -1,33 +1,39 @@
 if (!require(HelpersMG)) install.packages('HelpersMG')
+#required for BatchGetSymbols
+#requires apt-get install libxml2-dev
+if (!require(rvest)) install.packages('rvest')
+if (!require(XML)) install.packages('XML')
 if (!require(BatchGetSymbols)) install.packages('BatchGetSymbols')
 if (!require(future)) install.packages('future')
 if (!require(anytime)) install.packages('anytime')
 if (!require(RCurl)) install.packages('RCurl')
+if (!require(data.table)) install.packages('data.table')
 
-options("download.file.method"="wget")
+betaTestCoefficient = .25
+
+#options("download.file.method"="wget")
 
 library(HelpersMG)
 library(BatchGetSymbols)
 library(future)
+library(data.table)
 #library(anytime)
 #library(RCurl)
 
-options(clustermq.scheduler = "multicore")
-make(my_plan, parallelism = "clustermq", jobs = 4)
-
-# set tickers
 wget("ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqtraded.txt")
+#9 quarters is 5479/8897 61% (60%)
 nasdaqTraded <- head(read.csv("nasdaqtraded.txt",sep="|")$Symbol,-2)
 
-wget("ftp://ftp.nasdaqtrader.com/SymbolDirectory/bondslist.txt")
-bonds <- head(read.csv("bondslist.txt",sep="|")$Symbol,-2)
+#wget("ftp://ftp.nasdaqtrader.com/SymbolDirectory/bondslist.txt")
+#bonds <- head(read.csv("bondslist.txt",sep="|")$Symbol,-2)
 
 fil <- c()
 fil <- tempfile()
 first.date <- Sys.Date() - 821
 last.date <- Sys.Date() - 814
-#future::plan(future::multisession, workers = 4)
-dput(BatchGetSymbols(tickers = nasdaqTraded,
+future::plan(future::multisession, workers = 4)
+#using six sigma
+dput(BatchGetSymbols(tickers = sample(nasdaqTraded,770*betaTestCoefficient),
                      do.parallel = TRUE,
                      first.date = first.date,
                      last.date = last.date, 
@@ -37,11 +43,7 @@ dput(BatchGetSymbols(tickers = nasdaqTraded,
                      do.cache=FALSE),
      fil)
 
-
-filtered <- unique(dget(fil, keep.source = TRUE)$df.tickers$ticker)
-
-#25%
-len(filtered)
+filteredNasdaq <- unique(dget(fil, keep.source = TRUE)$df.tickers$ticker)
 
 first.date <- Sys.Date() - 821
 last.date <- Sys.Date()
@@ -49,8 +51,9 @@ last.date <- Sys.Date()
 fil <- c()
 fil <- tempfile()
 
-dput(BatchGetSymbols(tickers = sample(filtered,200), first.date = first.date,last.date = last.date, do.parallel = TRUE, do.cache=FALSE),fil ) # cache in tempdir(), fil)
+dput(BatchGetSymbols(tickers = sample(filteredNasdaq,220*betaTestCoefficient), first.date = first.date,last.date = last.date, do.parallel = TRUE, do.cache=FALSE),fil ) # cache in tempdir(), fil)
 
-fwrite(dget(fil, keep.source = TRUE)$df.tickers, "200Symbols2Years.csv")          
+dget(fil, keep.source = TRUE)$df.tickers
+fwrite(dget(fil, keep.source = TRUE)$df.tickers, "200NasdaqSymbols2Years.csv")          
 
 
