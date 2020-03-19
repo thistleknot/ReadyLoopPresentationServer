91f5e15aab21708416214368c1680c577085edb0
My digital passions <laferrierejc@gmail.com>
2020-03-19 08:58:26 -0600

    Update symbols3.R

--- symbols3.R
+++ symbols3.R
@@ -2,13 +2,18 @@ if (!require(HelpersMG)) install.packages('HelpersMG')
 if (!require(BatchGetSymbols)) install.packages('BatchGetSymbols')
 if (!require(future)) install.packages('future')
 if (!require(anytime)) install.packages('anytime')
+if (!require(RCurl)) install.packages('RCurl')
 
 options("download.file.method"="wget")
 
 library(HelpersMG)
 library(BatchGetSymbols)
 library(future)
-library(anytime)
+#library(anytime)
+#library(RCurl)
+
+options(clustermq.scheduler = "multicore")
+make(my_plan, parallelism = "clustermq", jobs = 4)
 
 # set tickers
 wget("ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqtraded.txt")
@@ -21,7 +26,7 @@ fil <- c()
 fil <- tempfile()
 first.date <- Sys.Date() - 821
 last.date <- Sys.Date() - 814
-future::plan(future::multisession, workers = floor(parallel::detectCores()*.75))
+#future::plan(future::multisession, workers = 4)
 dput(BatchGetSymbols(tickers = nasdaqTraded,
                      do.parallel = TRUE,
                      first.date = first.date,
