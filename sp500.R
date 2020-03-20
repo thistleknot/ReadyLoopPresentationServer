407210a7d1d49df75579de33120d8c3a208aead4
My digital passions <laferrierejc@gmail.com>
2020-03-19 18:39:00 -0600

    Update sp500.R
    this function will be sourced inside symbols3.R and as such will no longer need to specify the date.

--- sp500.R
+++ sp500.R
@@ -1,32 +1,31 @@
+#called from getSymbols3.R
+
 # for scraping
 library(rvest)
 # blanket import for core tidyverse packages
-library(tidyverse)
+if (!require(alphavantager)) install.packages('alphavantager')
+if (!require(timetk)) install.packages('timetk')
+if (!require(timeDate)) install.packages('timeDate')
+if (!require(dbplyr)) install.packages('dbplyr')
+if (!require(reprex)) install.packages('reprex')
+if (!require(tidyverse)) install.packages('tidyverse')
+
+if (!require(tidyquant)) install.packages('tidyquant')
+
 # tidy financial analysis 
-library(tidyquant)
+if (!require(janitor)) install.packages('janitor')
+
 # tidy data cleaning functions
+library(tidyverse)
 library(janitor)
 library(rvest)
+library(tidyquant)
 
 get_symbols = function(ticker = "CL"){
-  df = tq_get(ticker, from = date) %>% mutate(symbol = rep(ticker, length(date)))
+  df = tq_get(ticker, from = first.date) %>% mutate(symbol = rep(ticker, length(first.date)))
 }
 
 #https://towardsdatascience.com/exploring-the-sp500-with-r-part-1-scraping-data-acquisition-and-functional-programming-56c9498f38e8
-
-# save current system date to a variable
-today <- Sys.Date()
-# subtract 3 months from the current date
-date = today %m+% months(-25)
-print(date)
-
-# pass SP500 ticker ^GSPC to tq_get function
-
-#ftp://ftp.nasdaqtrader.com/SymbolDirectory/
-  
-#one_ticker = tq_get("^GSPC", from = date)
-#one_ticker %>% 
-#  head()
 
 # get the URL for the wikipedia page with all SP500 symbols
 url <- "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
@@ -42,8 +41,8 @@ tickers <- url %>%
 #create a vector of tickers
 sp500tickers <- tickers[[1]]
 sp500tickers = sp500tickers %>% mutate(Symbol = case_when(Symbol == "BRK.B" ~ "BRK-B",
-                                             Symbol == "BF.B" ~ "BF-B",
-                                             TRUE ~ as.character(Symbol)))
+                                                          Symbol == "BF.B" ~ "BF-B",
+                                                          TRUE ~ as.character(Symbol)))
 symbols <- sp500tickers$Symbol
 
 tickers_df = map(symbols, get_symbols) %>% bind_rows()
